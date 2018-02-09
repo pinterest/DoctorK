9f38c0f99d4f51482bf20f2b31604fafef735b60
Yu Yang <yuyang@pinterest.com>
2018-02-08 22:22:46 -0800

    update pom.xml etc. for kafka 1.0.0 client lib

--- kafkastats/src/main/java/com/pinterest/doctorkafka/util/OperatorUtil.java
+++ kafkastats/src/main/java/com/pinterest/doctorkafka/util/OperatorUtil.java
@@ -16,7 +16,7 @@ import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.network.ListenerName;
-import org.apache.kafka.common.protocol.SecurityProtocol;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import scala.Tuple2;
