119a3ec0087c3627ee5917bbda66a115f00829ce
Yu Yang <yuyang@pinterest.com>
2018-11-27 12:45:50 -0800

    bump package versions for a set of depdendent packages

--- drkafka/scripts/run_dev.sh
+++ drkafka/scripts/run_dev.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-java -cp drkafka/target/lib/*:drkafka/target/doctorkafka-0.2.4.jar \
+java -cp drkafka/target/lib/*:drkafka/target/doctorkafka-0.2.4.1.jar \
      -Dlog4j.configurationFile=file:./drkafka/config/log4j2.dev.xml   \
      com.pinterest.doctorkafka.DoctorKafkaMain server drkafka/config/doctorkafka.dev.yaml
