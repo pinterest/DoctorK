870af52eb29ec47e997c427795eb580b281ee910
kabochya <contact@pingminlin.com>
2019-05-07 08:40:23 -0700

    0.2.4.6

--- drkafka/scripts/action_retriever.sh
+++ drkafka/scripts/action_retriever.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-java -cp drkafka/target/lib/*:drkafka/target/doctorkafka-0.2.4.5.jar \
+java -cp drkafka/target/lib/*:drkafka/target/doctorkafka-0.2.4.6.jar \
      -Dlog4j.configurationFile=file:./drkafka/config/log4j2.dev.xml   \
      com.pinterest.doctorkafka.tools.DoctorKafkaActionRetriever   \
      $1 $2 $3 $4 $5 $6
